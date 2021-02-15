58c1b57a96d8ee5c6f27546fc4c5058240682218
Lefted <gartnermoritz@yahoo.de>
2021-02-15 12:57:42 +0100

    Use Provider with favorite feature

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../models/product.dart';
+import 'product_provider.dart';
 
 class ProductsProvider with ChangeNotifier {
   List<Product> _items = [
