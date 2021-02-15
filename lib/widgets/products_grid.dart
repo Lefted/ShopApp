31d4c63c18d5876a6f3394f021b725d19cb2db65
Lefted <gartnermoritz@yahoo.de>
2021-02-15 13:36:26 +0100

    Use alternative Provider constructor

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -13,13 +13,9 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, index) => ChangeNotifierProvider(
-        create: (ctx) => products[index],
-        child: ProductItem(
-          // products[index].id,
-          // products[index].title,
-          // products[index].imageUrl,
-        ),
+      itemBuilder: (ctx, index) => ChangeNotifierProvider.value(
+        value: products[index],
+        child: ProductItem(),
       ),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
