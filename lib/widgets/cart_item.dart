154bddd40aad12eb8d5e40d89ffdcfe1eef5eb68
Lefted <gartnermoritz@yahoo.de>
2021-02-18 10:34:47 +0100

    Add AltertDialog When Deleting Product From Cart

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -30,6 +30,29 @@ class CartItem extends StatelessWidget {
           horizontal: 15,
         ),
       ),
+      confirmDismiss: (_) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: [
+              FlatButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  Navigator.of(ctx).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       onDismissed: (_) {
         Provider.of<CartProvider>(context, listen: false).removeItem(productId);
       },
