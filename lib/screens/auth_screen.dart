4fd695f41dab1a82cbe7bd808ee806d73dc3b5e4
Lefted <gartnermoritz@yahoo.de>
2021-03-21 18:52:03 +0100

    Use AnimatedContainer in Authentication Screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -220,17 +220,9 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: AnimatedBuilder(
-        animation: _heightAnimation,
-        builder: (ctx, child) => Container(
-            // height: _authMode == AuthMode.Signup ? 320 : 260,
-            height: _heightAnimation.value.height,
-            constraints:
-                // BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-                BoxConstraints(minHeight: _heightAnimation.value.height),
-            width: deviceSize.width * 0.75,
-            padding: EdgeInsets.all(16.0),
-            child: child),
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeIn,
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
@@ -307,6 +299,17 @@ class _AuthCardState extends State<AuthCard>
             ),
           ),
         ),
+        // child: AnimatedBuilder(
+        //   animation: _heightAnimation,
+        //   builder: (ctx, child) => Container(
+
+        height: _authMode == AuthMode.Signup ? 320 : 260,
+        // height: _heightAnimation.value.height,
+        constraints:
+            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
+        // BoxConstraints(minHeight: _heightAnimation.value.height),
+        width: deviceSize.width * 0.75,
+        padding: EdgeInsets.all(16.0),
       ),
     );
   }
